d0dc180f6d39837c632123829d68e8fcf6759c51
mohamed khaled <mohakhaled324@gmail.com>
2026-03-17 18:26:04 +0200

    login screen 0.1

--- lib/features/auth/ui/screens/login_screen.dart
+++ lib/features/auth/ui/screens/login_screen.dart
@@ -1,15 +1,46 @@
-import 'package:coding_developer_driver_app/core/constants/shared_pref_keys.dart';
-import 'package:coding_developer_driver_app/core/services/shared_pref_service.dart';
+import 'package:coding_developer_driver_app/core/theming/colors.dart';
+import 'package:coding_developer_driver_app/core/widgets/custom_button_widget.dart';
+import 'package:coding_developer_driver_app/core/widgets/custom_text_form_field.dart';
+import 'package:coding_developer_driver_app/features/auth/ui/widgets/custom_login_or_register.dart';
+import 'package:coding_developer_driver_app/features/auth/ui/widgets/header_widget.dart';
+import 'package:coding_developer_driver_app/features/auth/ui/widgets/login_form.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 
 class LoginScreen extends StatelessWidget {
-  const LoginScreen({super.key});
+  LoginScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("${SharedPrefService.getString(SharedPrefKeys.language)}"),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(24.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(height: 100),
+                HeaderWidget(),
+                SizedBox(height: 50),
+                LoginForm(),
+                SizedBox(height: 5),
+                CustomButtonWidget(
+                  onTap: () {
+                    print("login");
+                  },
+                  label: "Login",
+                  buttonColor: AppColors.primaryColor,
+                ),
+                SizedBox(height: 50),
+                CustomLoginOrRegister(
+                  askForAccount: "Don't Have An Account?",
+                  answer: "Register",
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
